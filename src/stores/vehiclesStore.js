aa90ebdd747ffa669f3f9666062f223de590105d
Genci <genci@uniggardin.dk>
2019-06-15 13:51:45 +0200

    wip

--- src/stores/vehiclesStore.js
+++
@@ -1,7 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from 'redux-thunk';
-import vehicleReducer from '../reducers/vehicleReducer';
-
-const vehicleStore = createStore(vehicleReducer, applyMiddleware(thunk));
-
-export default vehicleStore;
